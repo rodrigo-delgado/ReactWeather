9f0d07293f5488715ee97295e2445b444c3571e0
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-07 19:30:05 +1300

    crate pages and new links components for navbar

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,18 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var Main = require('Main')
+var Weather = require('Weather')
+var About = require('About')
+var Examples = require('Examples')
 //Below is es6 object destructuring
 var {Route, Router, IndexRoute, hashHistory} = require('react-router')
 
   ReactDOM.render(
     <Router history={hashHistory}>
       <Route path='/' component={Main}>
-
+        <Route path='about' component={About}/>
+        <Route path='examples' component={Examples}/>
+        <IndexRoute component={Weather}/>
       </Route>
     </Router>,
     document.getElementById('app')
