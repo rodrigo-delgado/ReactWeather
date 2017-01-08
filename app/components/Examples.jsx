74aa998aa1783667fecc91904931b4f697881174
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-08 19:32:10 +1300

    add some examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,25 @@
 var React = require('react')
+var {Link} = require('react-router')
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page!</p>
+      <h1 className='text-center'>Examples</h1>
+      <p> Here are a few example location to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Santiago'>Santiago - Chile</Link>
+        </li>
+        <li>
+          <Link to='/?location=Wellington'>Wellington - New Zealand</Link>
+        </li>
+        <li>
+          <Link to='/?location=London'>London - UK</Link>
+        </li>
+        <li>
+          <Link to='/?location=Sydney'>Sydney - Australia</Link>
+        </li>
+      </ol>
     </div>
 
   )
