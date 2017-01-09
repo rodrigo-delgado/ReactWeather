e1ff8783236a8a314e8694285a5799fc6ae71a8d
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-10 08:18:29 +1300

    add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -9,14 +9,14 @@ module.exports = {
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`
 
     return axios.get(requestUrl).then(function (res) {
-      
+
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message)
       } else {
         return res.data.main.temp
       }
-    }, function (res) {
-      throw new Error(res.data.message)
+    }, function (err) {
+      throw new Error('Unable to fetch weather for that location')
     })
   }
 }
