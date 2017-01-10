529e0194841536fb40cfceec497d6d714594f3a6
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-10 17:50:09 +1300

    add sass to the app

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router')
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation()
 
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
   ReactDOM.render(
     <Router history={hashHistory}>
